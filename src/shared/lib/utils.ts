6f710bd824c04e2abe65b7fb4f3f50119a3e5d96
서주희 <maywngml@naver.com>
2025-04-14 04:15:34 +0900

    feat: 엘리먼트를 생성하는 함수 구현

---
+++ src/shared/lib/utils.ts
@@ -0,0 +1,15 @@
+export const createElement = <T>(
+  root: HTMLElement,
+  tagType: keyof HTMLElementTagNameMap,
+  options?: Partial<T>,
+) => {
+  const element = document.createElement(tagType);
+
+  if (options) {
+    Object.entries(options).forEach(([key, value]) => {
+      (element as any)[key] = value;
+    });
+  }
+
+  root.appendChild(element);
+};
