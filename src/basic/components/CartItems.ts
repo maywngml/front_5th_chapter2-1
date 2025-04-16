9ab5c94dc482138e4fe1163570eadf6bebe4205f
서주희 <maywngml@naver.com>
2025-04-17 02:45:28 +0900

    fix: 상품 수량이 제대로 표시되지 않는 문제 수정

--- src/basic/components/CartItems.ts
+++ src/basic/components/CartItems.ts
@@ -12,8 +12,8 @@ export default function CartItems() {
         .map(
           (
             item,
-          ) => `<div id=${item.id} className="flex justify-between items-center mb-2">
-        <span> ${item.name} - ${item.price}원 x 1</span><div><button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${item.id}" data-change="-1">-</button><button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${item.id}" data-change="1">+</button><button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="${item.id}">삭제</button></div>
+          ) => `<div id=${item.id} class="flex justify-between items-center mb-2">
+        <span> ${item.name} - ${item.price}원 x ${item.quantity}</span><div><button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${item.id}" data-change="-1">-</button><button class="quantity-change bg-blue-500 text-white px-2 py-1 rounded mr-1" data-product-id="${item.id}" data-change="1">+</button><button class="remove-item bg-red-500 text-white px-2 py-1 rounded" data-product-id="${item.id}">삭제</button></div>
         </div>`,
         )
         .join('')}
