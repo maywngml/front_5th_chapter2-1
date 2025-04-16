b96dde758d1c1147bb873fa5b34939c43d2ed432
서주희 <maywngml@naver.com>
2025-04-16 22:52:11 +0900

    feat: Select 공통 컴포넌트 구현

---
+++ src/basic/components/Select.ts
@@ -0,0 +1,32 @@
+interface Option extends HTMLOptionElement {
+  id: string;
+  value: string;
+  content: string;
+}
+
+interface SelectProps {
+  id: string;
+  className: string;
+  options: Option[];
+}
+
+export default function Select({ id, className, options }: SelectProps) {
+  const select = document.createElement('select');
+  select.id = id;
+  select.className = className;
+
+  const render = () => {
+    select.innerHTML = /* HTML */ `
+      ${options
+        .map(
+          ({ id, value, content }: Option) =>
+            `<option id=${id} value=${value}>${content}</option>`,
+        )
+        .join('')}
+    `;
+  };
+
+  render();
+
+  return select;
+}
