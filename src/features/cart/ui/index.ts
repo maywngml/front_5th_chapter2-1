4c9d2f07e44f5f3b3f1c02c8aea5fe9c04cb3a49
서주희 <maywngml@naver.com>
2025-04-17 19:16:59 +0900

    chore: 불필요한 파일 및 폴더 제거

--- src/features/cart/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as renderCartTotal } from './renderCartTotal';
-export { default as renderProductSelect } from './renderProductSelect';
-export { default as updateBonusPoints } from './updateBonusPoints';
-export { default as renderTotalPrice } from './renderTotalPrice';
-export { default as updateProductSelectOptions } from './updateProductSelectOptions';
