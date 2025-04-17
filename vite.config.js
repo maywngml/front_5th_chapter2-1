c859b3180e28b05ed694974f94e1017e430615ed
서주희 <maywngml@naver.com>
2025-04-17 20:34:57 +0900

    chore: 리액트 설치 및 관련 환경 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 
+const isCI = process.env.CI === 'true';
+
 export default defineConfig({
+  base: isCI ? '/' : '/front_5th_chapter1-3/',
+  plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
   },
