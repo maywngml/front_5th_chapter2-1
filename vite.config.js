ebda3a622a40b380e79e9ced7929db805c61fcf0
서주희 <maywngml@naver.com>
2025-04-17 23:54:15 +0900

    chore: gh-pages 설치, deploy 명령어 추가, 빌드 설정

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 const isCI = process.env.CI === 'true';
 
 export default defineConfig({
-  base: isCI ? '/' : '/front_5th_chapter1-3/',
+  base: isCI ? '' : '/front_5th_chapter2-1/',
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        basic: path.resolve(__dirname, 'index.basic.html'),
+        advanced: path.resolve(__dirname, 'index.advanced.html'),
+      },
+    },
   },
   test: {
     globals: true,
