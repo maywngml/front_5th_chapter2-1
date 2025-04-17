b18500641a5eae9adaf44a2e4ff9a05d3eca86b4
서주희 <maywngml@naver.com>
2025-04-17 18:31:33 +0900

    feat: 세일 공지 함수들 Cart 컴포넌트 안으로 이동, main.basic 기존 로직들 모두 제거

--- src/basic/components/Cart.ts
+++ src/basic/components/Cart.ts
@@ -2,12 +2,59 @@ import CartItems from './CartItems';
 import ProductSelect from './ProductSelect';
 import CartTotal from './CartTotal';
 import StockStatus from './StockStatus';
+import { useProductsStore } from '../stores';
 
 export default function Cart() {
   const wrapper = document.createElement('div');
   wrapper.id = 'wrapper';
   wrapper.className =
     'max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8';
+
+  // 랜덤하게 선택된 상품에 대해 세일이 진행 중임을 알립니다.
+  const notifyLuckySale = () => {
+    const { products, updateProducts } = useProductsStore();
+
+    setInterval(() => {
+      // 랜덤으로 세일을 진행할 상품을 선택합니다.
+      const luckyItem = products[Math.floor(Math.random() * products.length)];
+
+      // 해당 상품의 재고가 남아있다면 상품 가격의 20%를 할인하고 알림창을 띄웁니다.
+      if (luckyItem.quantity > 0) {
+        alert(`번개세일! ${luckyItem.name}이(가) 20% 할인 중입니다!`);
+        updateProducts(luckyItem.id, {
+          price: Math.round(luckyItem.price * 0.8),
+        });
+      }
+    }, 10000);
+  };
+
+  // 재고가 남아있는 상품 중 하나를 선택해 추가 할인을 적용합니다.
+  const notifyPurchaseSuggestion = () => {
+    const { products, lastSelectedProductId, updateProducts } =
+      useProductsStore();
+
+    setInterval(() => {
+      if (!lastSelectedProductId) return;
+
+      // 재고가 있으며 마지막에 추가된 상품이 아닌 다른 상품을 선택합니다.
+      const suggestedProduct = products.find(
+        (product) =>
+          product.id !== lastSelectedProductId && product.quantity > 0,
+      );
+
+      // 위 조건에 부합하는 상품이 있을 경우 해당 상품 가격을 변경하고 알림창을 생성합니다.
+      if (suggestedProduct) {
+        suggestedProduct.price = Math.round(suggestedProduct.price * 0.95);
+
+        alert(
+          `${suggestedProduct.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`,
+        );
+        updateProducts(suggestedProduct.id, {
+          price: Math.round(Math.round(suggestedProduct.price * 0.95)),
+        });
+      }
+    }, 60000);
+  };
 
   const render = () => {
     wrapper.innerHTML = /* HTML */ `
@@ -20,6 +67,8 @@ export default function Cart() {
   };
 
   render();
+  notifyLuckySale();
+  notifyPurchaseSuggestion();
 
   return wrapper;
 }
--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,88 +1,8 @@
-import { createElement } from '../shared/lib/utils';
-import { products } from '../shared/config/product';
 import { HomePage } from './pages';
 
-// TODO: 파일 분리
-let lastSelectedProductId;
-
-// 화면에 돔을 그리는 함수입니다.
-const renderElement = () => {
+const main = () => {
   // 페이지 컴포넌트를 호출합니다.
   HomePage();
-};
-
-// TODO: 함수 주석은 더 상세하게 달기
-// 랜덤하게 선택된 상품에 대해 세일이 진행 중임을 알립니다.
-const notifyLuckySale = () => {
-  setInterval(() => {
-    // 랜덤으로 세일을 진행할 상품을 선택합니다.
-    const luckyItem = products[Math.floor(Math.random() * products.length)];
-
-    // 해당 상품의 재고가 남아있다면 가격을 변경하고 알림창을 띄웁니다.
-    if (luckyItem.quantity > 0) {
-      luckyItem.price = Math.round(luckyItem.price * 0.8);
-
-      alert(`번개세일! ${luckyItem.name}이(가) 20% 할인 중입니다!`);
-      updateSelectOptions(luckyItem.id);
-    }
-  }, 30000);
-};
-
-// 재고가 남아있는 상품 중 하나를 선택해 추가 할인을 적용합니다.
-const notifyPurchaseSuggestion = () => {
-  setInterval(() => {
-    if (!lastSelectedProductId) return;
-
-    // 재고가 있으며 마지막에 추가된 상품이 아닌 다른 상품을 선택합니다.
-    const suggestedProduct = products.find(
-      (product) => product.id !== lastSelectedProductId && product.quantity > 0,
-    );
-
-    // 위 조건에 부합하는 상품이 있을 경우 해당 상품 가격을 변경하고 알림창을 생성합니다.
-    if (suggestedProduct) {
-      suggestedProduct.price = Math.round(suggestedProduct.price * 0.95);
-
-      alert(
-        `${suggestedProduct.name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`,
-      );
-      updateSelectOptions(suggestedProduct.id);
-    }
-  }, 60000);
-};
-
-// 상품 선택 옵션을 업데이트합니다.
-const updateSelectOptions = (productId) => {
-  const select = document.getElementById('product-select');
-
-  // productId가 빈 값이 아니라면 해당 상품의 옵션만 변경합니다.
-  if (productId) {
-    const product = products.find((product) => product.id === productId);
-    const option = document.querySelector(
-      `#product-select #product-select-option-${productId}`,
-    );
-
-    option.textContent = `${product.name} - ${product.price}원`;
-    option.disabled = !!(product.quantity === 0);
-  } else {
-    // productId가 빈 값이라면 전체 상품 옵션을 새로 설정합니다.
-    select.innerHTML = '';
-
-    products.forEach((product) => {
-      createElement(select, 'option', {
-        id: `product-select-option-${product.id}`,
-        value: product.id,
-        textContent: `${product.name} - ${product.price}원`,
-        disabled: !!(product.quantity === 0),
-      });
-    });
-  }
-};
-
-const main = () => {
-  renderElement();
-  updateSelectOptions();
-  // notifyLuckySale();
-  // notifyPurchaseSuggestion();
 };
 
 main();
