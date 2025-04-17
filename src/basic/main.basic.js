548cb9cd99badef21daa9f878dc0dea8f9fc2d63
서주희 <maywngml@naver.com>
2025-04-17 17:39:31 +0900

    refactor: 컴포넌트에 구현되어 있는 이벤트, 함수 등은 제거

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -87,109 +87,6 @@ const updateSelectOptions = (productId) => {
   }
 };
 
-// 현재 장바구니 속 아이템의 총 개수와 조건에 따라 할인율을 계산해 반환합니다.
-const getDiscountRate = (totalCount, totalPrice, totalDiscountedPrice) => {
-  let discountRate = 0;
-
-  // 총 수량이 30개 이상일 경우 추가 할인 적용 여부를 검토합니다.
-  if (totalCount >= 30) {
-    const currentDiscount = totalPrice - totalDiscountedPrice;
-    // 기존 할인 금액보다 25% 할인이 더 클 경우, 25% 할인을 적용합니다.
-    if (totalDiscountedPrice * 0.25 > currentDiscount) {
-      totalDiscountedPrice = totalPrice * (1 - 0.25);
-      discountRate = 0.25;
-    } else {
-      discountRate = (totalPrice - totalDiscountedPrice) / totalPrice;
-    }
-  } else {
-    discountRate = (totalPrice - totalDiscountedPrice) / totalPrice;
-  }
-
-  // 화요일에는 특별 할인이 적용됩니다.
-  if (new Date().getDay() === 2) {
-    totalDiscountedPrice *= 1 - 0.1;
-    discountRate = Math.max(discountRate, 0.1);
-  }
-
-  return discountRate;
-};
-
-// 장바구니에 담긴 아이템들의 총 가격, 개수, 할인된 가격을 계산합니다.
-const calcTotalCart = () => {
-  const cartItems = document.getElementById('cart-items');
-  // 장바구니에 담긴 아이템들이 담긴 엘리먼트 배열
-  const cartItemElements = cartItems.children;
-  // 총 가격, 개수, 할인된 가격을 담는 변수들입니다.
-  let totalPrice = 0,
-    totalCount = 0,
-    totalDiscountedPrice = 0;
-
-  // 장바구니에 담긴 아이템의 가격과 할인율을 확인해 총 가격, 개수, 할인된 가격을 계산합니다.
-  for (const cartItemElement of cartItemElements) {
-    const currentItem = products.find(
-      (product) => product.id === cartItemElement.id,
-    );
-    const currentCount = parseInt(
-      cartItemElement.querySelector('span').textContent.split('x ')[1],
-    );
-    const currentTotalPrice = currentItem.price * currentCount;
-    const currentDiscountRate =
-      currentCount >= 10 ? currentItem.discountRate : 0;
-
-    totalCount += currentCount;
-    totalPrice += currentTotalPrice;
-    totalDiscountedPrice += currentTotalPrice * (1 - currentDiscountRate);
-  }
-
-  return { totalPrice, totalCount, totalDiscountedPrice };
-};
-
-// 장바구니에 총액과 할인율을 표시합니다.
-const renderTotalPrice = (totalDiscountedPrice, discountRate) => {
-  const cartTotal = document.getElementById('cart-total');
-
-  // 최종 금액을 화면에 표시합니다.
-  cartTotal.textContent = `총액: ${Math.round(totalDiscountedPrice)}원`;
-
-  // 할인이 적용될 경우에만 할인율을 화면에 표시합니다.
-  if (discountRate > 0) {
-    createElement(cartTotal, 'span', {
-      className: BASE_STYLES.CART_TOTAL,
-      textContent: `(${(discountRate * 100).toFixed(1)}% 할인 적용)`,
-    });
-  }
-};
-
-// 장바구니 정보를 업데이트합니다.
-const updateCart = () => {
-  const { totalPrice, totalCount, totalDiscountedPrice } = calcTotalCart();
-  const discountRate = getDiscountRate(
-    totalCount,
-    totalPrice,
-    totalDiscountedPrice,
-  );
-
-  renderTotalPrice(totalDiscountedPrice, discountRate);
-  updateStockInfo();
-  renderBonusPoints(totalDiscountedPrice);
-};
-
-// 총액에 따른 보너스 포인트 점수를 계산하고 화면에 보여줍니다.
-const renderBonusPoints = (totalAmount) => {
-  const cartTotal = document.getElementById('cart-total');
-  const bonusPoints = Math.floor(totalAmount / 1000);
-  let pointsTag = document.getElementById('bonus-points');
-
-  if (!pointsTag) {
-    pointsTag = createElement(cartTotal, 'span', {
-      id: 'bonus-points',
-      className: BASE_STYLES.LOYALTY_POINTS,
-    });
-  }
-
-  pointsTag.textContent = `(포인트: ${bonusPoints})`;
-};
-
 // 상품들의 재고 정보를 업데이트합니다.
 const updateStockInfo = () => {
   const stockInfo = document.getElementById('stock-status');
@@ -206,73 +103,11 @@ const updateStockInfo = () => {
   stockInfo.textContent = infoMsg;
 };
 
-// 장바구니 내 상품 수량을 수정하는 함수입니다.
-const changeProductQuantityInCart = (change, element, product) => {
-  const changeQuantity = parseInt(change);
-  let textContent = element.querySelector('span').textContent;
-  const newQuantity = parseInt(textContent.split('x ')[1]) + changeQuantity;
-
-  // 변경될 수량이 0과 같거나 작다면 장바구니에서 해당 상품을 제거합니다.
-  if (newQuantity <= 0) {
-    element.remove();
-    product.quantity -= changeQuantity;
-  } else if (changeQuantity <= product.quantity) {
-    // 상품 재고가 충분하다면 수량을 변경합니다.
-    element.querySelector('span').textContent = textContent.replace(
-      /x\s*\d+$/,
-      `x ${newQuantity}`,
-    );
-    product.quantity -= changeQuantity;
-  } else {
-    // 재고가 부족하다면 알림창을 표시합니다.
-    alert('재고가 부족합니다.');
-  }
-};
-
-// 장바구니에서 상품을 제거하는 함수입니다.
-const removeProductInCart = (element, product) => {
-  const removeQuantity = parseInt(
-    element.querySelector('span').textContent.split('x ')[1],
-  );
-  product.quantity += removeQuantity;
-  element.remove();
-};
-
-// 장바구니 내 상품 영역을 클릭했을 때 실행되는 핸들러 함수입니다.
-const handleClickCartItems = (event) => {
-  const target = event.target;
-  if (
-    target.classList.contains('quantity-change') ||
-    target.classList.contains('remove-item')
-  ) {
-    const productId = target.dataset.productId;
-    const productElement = document.getElementById(productId);
-    const product = products.find((product) => product.id === productId);
-
-    if (target.classList.contains('quantity-change')) {
-      changeProductQuantityInCart(
-        target.dataset.change,
-        productElement,
-        product,
-      );
-    } else if (target.classList.contains('remove-item')) {
-      removeProductInCart(productElement, product);
-    }
-
-    updateCart();
-  }
-};
-
 const main = () => {
   renderElement();
   updateSelectOptions();
-  updateCart();
   notifyLuckySale();
   notifyPurchaseSuggestion();
 };
 
 main();
-
-document
-  .getElementById('cart-items')
-  .addEventListener('click', handleClickCartItems);
